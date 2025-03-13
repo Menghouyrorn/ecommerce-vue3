44a8c1e6026bfbf4dcb9590190187f2c9e124372
Menghouyrorn <menghouyrorn@gmail.com>
2025-03-13 09:26:31 +0700

    feat/about:create about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    About Page
+  <div class="space-y-2">
+    <div class="space-y-2">
+      <h1 class="font-bold text-2xl text-center uppercase">About Page</h1>
+      <div class="text-center w-[75%] m-auto">
+        Ecommerce Shop is an online store where customers can find products, browse offerings, and place purchases
+        online. It facilitates the transaction between a buyer and seller. A digital storefront can serve as the virtual
+        equivalent of the product shelves, sales staff, and cash register of a physical shop.
+      </div>
+    </div>
+    <div class="w-[70%] m-auto">
+      <img class="w-full" src="https://www.searchenginejournal.com/wp-content/uploads/2020/03/20-free-things-you-need-to-do-after-launching-your-ecommerce-website-5e664bcb60da5-1280x720.png" alt="image logo">
+    </div>
   </div>
 </template>
 
